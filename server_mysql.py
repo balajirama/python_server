19ce303168a89e484299962d94967c280ab11a42
Balaji Ramasubramanian <balaji.ramasubramanian@gmail.com>
2019-07-19 07:51:45 -0700

    Add commonly used queries to the server code

--- server_mysql.py
+++ server_mysql.py
@@ -11,6 +11,13 @@ bcrypt = Bcrypt(app)
 app.secret_key = 'darksecret'
 
 dbname = 'dbname'
+# The following queries can be re-used or edited before use
+# CRUD - Create, Read, Update, Delete
+CREATE_RECORD = 'INSERT INTO table ( col1, col2, created_at, updated_at ) VALUES ( %(col1)s, %(col2)s, NOW(), NOW() )'
+READ_ALL      = 'SELECT * from table'
+READ_ONE      = 'SELECT * from table WHERE id=%(id)s'
+UPDATE_RECORD = 'UPDATE table SET col1=%(col1)s, col2=%(col2)s, updated_at=NOW() WHERE id=%(id)s'
+DELETE_RECORD = 'DELETE FROM table WHERE id=%(id)s'
 
 @app.route("/")
 def mainpage():
