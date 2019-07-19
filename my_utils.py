b4236c1e14021d0830b79bf0ccdb18edfcafd8de
Balaji Ramasubramanian <balaji.ramasubramanian@gmail.com>
2019-07-19 08:48:46 -0700

    Make the my_utils module more intuitive

--- my_utils.py
+++ my_utils.py
@@ -3,8 +3,8 @@ from datetime import datetime
 import re
 
 EMAIL_REGEX = re.compile(r"^[a-zA-Z0-9._+-]+@[a-zA-Z0-9]+\.[a-zA-Z]+$")
-NUM_REGEX = re.compile(r"^.*[0-9]+.*")
-CAP_REGEX = re.compile(r"^.*[A-Z]+.*")
+AT_LEAST_ONE_NUM_REGEX = re.compile(r"^.*[0-9]+.*")
+AT_LEAST_ONE_CAP_REGEX = re.compile(r"^.*[A-Z]+.*")
 
 def is_age_over(num, dob):
     if(type(dob) == type('')):
@@ -35,15 +35,15 @@ def get_age(dob):
         age += 1
     return age
 
-def validate_password(fields, categories=['password', 'confirm']):
+def is_valid_password(request_form, categories=['password', 'confirm']):
     is_valid = True
-    if len(fields[categories[0]]) < 8:
+    if len(request_form[categories[0]]) < 8:
         flash("Password must be at least eight characters", categories[0])
         is_valid = False
-    if not(NUM_REGEX.match(fields[categories[0]]) and CAP_REGEX.match(fields[categories[0]])):
+    if not(AT_LEAST_ONE_NUM_REGEX.match(request_form[categories[0]]) and AT_LEAST_ONE_CAP_REGEX.match(request_form[categories[0]])):
         flash("Password must contain at least one capital letter and one digit", categories[0])
         is_valid = False
-    if (fields[categories[1]] == "") or (fields[categories[0]] != fields[categories[1]]):
+    if (request_form[categories[1]] == "") or (request_form[categories[0]] != request_form[categories[1]]):
         flash("Passwords do not match", categories[1])
         is_valid = False
     return is_valid
